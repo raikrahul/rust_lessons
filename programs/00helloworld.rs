9c7e1fc9102a8d7ec4e3f060cd78a2cb808c0ec3
stream-rahul <streaming.rahul.rai@gmail.com>
2025-01-26 01:07:59 +0530

    Create 00helloworld.rs

---
+++ programs/00helloworld.rs
@@ -0,0 +1,7 @@
+// Purpose: Test if Rust is properly installed and working
+// To compile: rustc 00helloworld.rs
+// To run: ./00helloworld (or 00helloworld.exe on Windows)
+
+fn main() {
+    println!("Hello, World!");
+}
